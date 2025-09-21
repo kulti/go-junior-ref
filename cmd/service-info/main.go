e5533d70c237fba5d904fe6df4bec88072dbf821
Aleksey Bakin <alexey.bakin@gmail.com>
2025-09-21 13:34:33 +0300

    feat(service-info): check version is deployed

--- cmd/service-info/main.go
+++ cmd/service-info/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 	"sort"
 	"strings"
 )
@@ -14,6 +16,7 @@ import (
 var (
 	errMissedCommandName  = errors.New("missed command name")
 	errUnknownCommandName = errors.New("unknown command name")
+	errMissedRequiredArg  = errors.New("missed require argument")
 )
 
 func main() {
@@ -34,6 +37,8 @@ func run() error {
 	switch cmdName {
 	case "versions":
 		return runVersions()
+	case "is-deployed":
+		return runIsDeployed()
 	default:
 		return fmt.Errorf("%w: %s", errUnknownCommandName, cmdName)
 	}
@@ -63,6 +68,63 @@ func runVersions() error {
 			continue
 		}
 		fmt.Printf("\033[0;32m%s  [OK] %s\033[0m\n", s.name, s.meta)
+	}
+
+	return nil
+}
+
+func runIsDeployed() error {
+	serviceNames := flag.String("s", "", "service name(s)")
+	commit := flag.String("c", "", "commit hash")
+	flag.Parse()
+
+	if *commit == "" {
+		return fmt.Errorf("%w: commit hash", errMissedRequiredArg)
+	}
+	commitMsg, err := commitMessage(*commit)
+	if err != nil {
+		return fmt.Errorf("get commit message: %w", err)
+	}
+
+	services := strings.Split(*serviceNames, ",")
+	if *serviceNames == "" {
+		services = allServices()
+	}
+
+	type state struct {
+		serviceState
+		deployed bool
+	}
+	states := make([]state, len(services))
+
+	for i, service := range services {
+		state := state{serviceState: fetchServiceState(service)}
+		serviceCommit := getCommitFromMeta(state.meta)
+		if serviceCommit != "" {
+			deployed, err := isAncestor(*commit, serviceCommit)
+			if err != nil {
+				state.err = "check is deployed: " + err.Error()
+			} else {
+				state.deployed = deployed
+			}
+		}
+
+		states[i] = state
+	}
+
+	fmt.Printf("Looking up for commit %s:\n%s\n", *commit, commitMsg)
+	fmt.Println("---------------")
+
+	for _, state := range states {
+		if state.err != "" {
+			fmt.Printf("\033[0;31m%s  [ERROR] (%v)\033[0m\n", state.name, state.err)
+		} else if state.unknown {
+			fmt.Printf("\033[1;33m%s  [UNKNOWN]\033[0m\n", state.name)
+		} else if state.deployed {
+			fmt.Printf("\033[0;32m%s  [DEPLOYED]\033[0m\n", state.name)
+		} else {
+			fmt.Printf("\033[0;37m%s  [NOT DEPLOYED] Current=%+v\033[0m\n", state.name, state.meta)
+		}
 	}
 
 	return nil
@@ -116,4 +178,49 @@ func fetchServiceState(serviceName string) serviceState {
 	}
 	serviceState.meta = string(body)
 	return serviceState
+}
+
+func getCommitFromMeta(meta string) string {
+	n := strings.Index(meta, "Build Version: ")
+	if n == -1 {
+		return ""
+	}
+
+	commit, _, _ := strings.Cut(meta[n+len("Build Version: "):], " ")
+	return commit
+}
+
+func isAncestor(a, b string) (bool, error) {
+	args := []string{"merge-base", "--is-ancestor", a, b}
+
+	cmd := exec.Command("git", args...)
+	errBuf := &bytes.Buffer{}
+	cmd.Stderr = errBuf
+
+	if err := cmd.Run(); err != nil {
+		var ee *exec.ExitError
+		if errors.As(err, &ee) && ee.ExitCode() == 1 {
+			return false, nil
+		}
+
+		return false, fmt.Errorf("%w (%s)", err, errBuf.Bytes())
+	}
+
+	return true, nil
+}
+
+func commitMessage(commit string) (string, error) {
+	args := []string{"log", "--format=%B", "-n", "1", commit}
+
+	cmd := exec.Command("git", args...)
+	outBuf := &bytes.Buffer{}
+	errBuf := &bytes.Buffer{}
+	cmd.Stdout = outBuf
+	cmd.Stderr = errBuf
+
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("%w (%s)", err, errBuf.Bytes())
+	}
+
+	return strings.TrimSpace(outBuf.String()), nil
 }
