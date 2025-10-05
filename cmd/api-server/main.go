4f9e20aab646348f01898bf2f1b47772f93a049e
Aleksey Bakin <alexey.bakin@gmail.com>
2025-10-05 14:07:18 +0300

    feat(api-server): implement CreateList

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,18 +1,72 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"runtime/debug"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	http.HandleFunc("/debug/info", handleServiceInfo)
+	conn, err := pgx.Connect(context.Background(), os.Getenv("API_SERVER_DB_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	http.HandleFunc("GET /debug/info", handleServiceInfo)
+	http.HandleFunc("POST /v1/lists", newHandeCreateList(conn))
 
 	fmt.Println("service run")
 	if err := http.ListenAndServe(":8090", nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func newHandeCreateList(conn *pgx.Conn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req struct {
+			Name string `json:"name"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			slog.Error("failed decode create list request", slog.String("err", err.Error()))
+			http.Error(w, "invalid body json", http.StatusBadRequest)
+			return
+		}
+
+		if req.Name == "" {
+			http.Error(w, "missed required field: name", http.StatusBadRequest)
+			return
+		}
+
+		listID := uuid.NewString()
+		if _, err := conn.Exec(r.Context(), `INSERT INTO lists(id, name) VALUES($1, $2)`, listID, req.Name); err != nil {
+			slog.Error("failed create list in db", slog.String("err", err.Error()))
+			http.Error(w, "internal error", http.StatusInternalServerError)
+			return
+		}
+
+		var resp struct {
+			List struct {
+				ID   string `json:"id"`
+				Name string `json:"name"`
+			} `json:"list"`
+		}
+
+		resp.List.ID = listID
+		resp.List.Name = req.Name
+
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			// how to handle this error?
+		}
 	}
 }
 
