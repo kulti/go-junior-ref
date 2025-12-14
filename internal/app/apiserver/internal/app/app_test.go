92e475af1a87fc91435cbaecbb142e8ce6f11ba1
Aleksey Bakin <alexey.bakin@gmail.com>
2025-12-14 16:32:31 +0300

    tests(api-server): app unit tests

---
+++ internal/app/apiserver/internal/app/app_test.go
@@ -0,0 +1,48 @@
+package app_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/kulti/task_list_course/internal/app/apiserver/internal/models"
+	"github.com/stretchr/testify/require"
+)
+
+//go:generate mockgen -destination mock_test.go -source=app.go -package=app_test -mock_names store=MockStore,publisher=MockPublisher
+
+func TestSubscribe(t *testing.T) {
+	t.Parallel()
+	f := newFixture(t)
+
+	listID := faker.UUIDHyphenated()
+	email := faker.Email()
+	f.ExpectStoreSubscribe(listID, email)
+
+	f.Subscribe(context.Background(), t, listID, email)
+}
+
+func TestGetList(t *testing.T) {
+	t.Parallel()
+	f := newFixture(t)
+
+	list := models.List{
+		ID:   faker.UUIDHyphenated(),
+		Name: faker.Word(),
+	}
+	f.ExpectStoreGetList(list)
+
+	retList := f.GetList(context.Background(), t, list.ID)
+	require.Equal(t, list, retList)
+}
+
+func TestCreateList(t *testing.T) {
+	t.Parallel()
+	f := newFixture(t)
+
+	listName := faker.Word()
+	listID := f.ExpectStoreCreateList(listName)
+
+	retListID := f.CreateList(context.Background(), t, listName)
+	require.Equal(t, *listID, retListID)
+}
---
+++ internal/app/apiserver/internal/app/fixture_test.go
@@ -0,0 +1,87 @@
+package app_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/kulti/task_list_course/internal/app/apiserver/internal/app"
+	"github.com/kulti/task_list_course/internal/app/apiserver/internal/models"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+type fixture struct {
+	app    *app.App
+	store  *MockStore
+	pubish *MockPublisher
+}
+
+func newFixture(t *testing.T) *fixture {
+	mockCtl := gomock.NewController(t)
+	store := NewMockStore(mockCtl)
+	publisher := NewMockPublisher(mockCtl)
+	app := app.New(app.Params{
+		Store:     store,
+		Publisher: publisher,
+	})
+	return &fixture{
+		app:    app,
+		store:  store,
+		pubish: publisher,
+	}
+}
+
+func (f *fixture) ExpectStoreSubscribe(listID, email string) {
+	f.store.EXPECT().Subscribe(gomock.Any(), listID, email).Return(nil)
+}
+
+func (f *fixture) ExpectStoreGetList(list models.List) {
+	f.store.EXPECT().GetList(gomock.Any(), list.ID).Return(list, nil)
+}
+
+func (f *fixture) ExpectStoreCreateList(listName string) *string {
+	var listID string
+	f.store.EXPECT().CreateList(gomock.Any(), createListMatcher{listName}).
+		Do(func(ctx context.Context, l models.List) {
+			listID = l.ID
+		})
+	return &listID
+}
+
+func (f *fixture) Subscribe(ctx context.Context, t *testing.T, listID, email string) {
+	err := f.app.Subscribe(ctx, listID, email)
+	require.NoError(t, err)
+}
+
+func (f *fixture) GetList(ctx context.Context, t *testing.T, listID string) models.List {
+	list, err := f.app.GetList(ctx, listID)
+	require.NoError(t, err)
+	return list
+}
+
+func (f *fixture) CreateList(ctx context.Context, t *testing.T, listName string) string {
+	listID, err := f.app.CreateList(ctx, listName)
+	require.NoError(t, err)
+	return listID
+}
+
+type createListMatcher struct {
+	name string
+}
+
+func (m createListMatcher) Matches(x any) bool {
+	l, ok := x.(models.List)
+	if !ok {
+		return false
+	}
+	return l.Name == m.name
+}
+
+func (m createListMatcher) String() string {
+	return fmt.Sprintf("models.List with Name=%s", m.name)
+}
+
+func (m createListMatcher) Got(x any) string {
+	return fmt.Sprintf("%#v", x)
+}
